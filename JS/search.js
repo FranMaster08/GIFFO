e9dd545bfbe112158cace4e9c6ac247d22d39070
Fedecba92 <71454887+Fedecba92@users.noreply.github.com>
2021-05-03 09:13:28 -0300

    added trending functions(modal)

--- JS/search.js
+++ JS/search.js
@@ -9,48 +9,195 @@
   //6. celebrar con los datos
 
 
-  const getSearchGifsByKeyword =  async (apiKey,keyword) => {
-    const API_URL = "https://api.giphy.com/v1/gifs/search";
-    try {
-      const tags = await fetch(`${API_URL}?api_key=${apiKey}&q=${keyword}&limit=12`);
-      return tags.json()
-    } catch (error) {
-      console.log("ocurrio un error",error)
-    }
-  }
-  const getSearchTags =  async (apiKey,query) => {
-    const API_URL = "https://api.giphy.com/v1/gifs/search/tags";
-    try {
-      const tags = await fetch(`${API_URL}?api_key=${apiKey}&q=${query}`);
-      return tags.json()
-    } catch (error) {
-      console.log("ocurrio un error",error)
-    }
-  }
+  // const getSearchGifsByKeyword =  async (apiKey,keyword) => {
+  //   const API_URL = "https://api.giphy.com/v1/gifs/search";
+  //   try {
+  //     const tags = await fetch(`${API_URL}?api_key=${apiKey}&q=${keyword}&limit=12`);
+  //     return tags.json()
+  //   } catch (error) {
+  //     console.log("ocurrio un error",error)
+  //   }
+  // }
+  // const getSearchTags =  async (apiKey,query) => {
+  //   const API_URL = "https://api.giphy.com/v1/gifs/search/tags";
+  //   try {
+  //     const tags = await fetch(`${API_URL}?api_key=${apiKey}&q=${query}`);
+  //     return tags.json()
+  //   } catch (error) {
+  //     console.log("ocurrio un error",error)
+  //   }
+  // }
   
-  const getSuggestionsHanlder = async (ev) => {
-  const API_KEY =  "LcybAN2NSdMZKawiiuEU0m7lgBTrf52c";
-    const contenedorSugerencias = document.querySelector('#match_list');
-    const contenedorGifs = document.querySelector('#suggestions');
-    contenedorSugerencias.innerHTML = '';
-    contenedorGifs.innerHTML = '';
-    if(ev.target.value.length >= 3 && ev.keyCode !== '13') {
-        const tags = await getSearchTags(API_KEY,ev.target.value);
-        tags.data.forEach(tag => {
+  // const getSuggestionsHanlder = async (ev) => {
+  // const API_KEY =  "LcybAN2NSdMZKawiiuEU0m7lgBTrf52c";
+  //   const contenedorSugerencias = document.querySelector('#match_list');
+  //   const contenedorGifs = document.querySelector('#suggestions');
+  //   contenedorSugerencias.innerHTML = '';
+  //   contenedorGifs.innerHTML = '';
+  //   if(ev.target.value.length >= 3 && ev.keyCode !== '13') {
+  //       const tags = await getSearchTags(API_KEY,ev.target.value);
+  //       tags.data.forEach(tag => {
+  //         const newLi = document.createElement('li');
+  //         newLi.textContent = tag.name;
+  //         contenedorSugerencias.appendChild(newLi);
+  //       });
+  //   }
+  //   if (ev.keyCode === 13) {
+  //     const gifs = await getSearchGifsByKeyword(API_KEY,ev.target.value);
+  //       gifs.data.forEach(gif => {
+  //         const newImg = document.createElement('img');
+  //         newImg.setAttribute("src",gif.images.fixed_height.url)
+  //         contenedorGifs.appendChild(newImg);
+  //       });
+  //   }
+  // }
+  // document.addEventListener('DOMContentLoaded',async () => {
+  //   document.querySelector('#buscador').addEventListener('keyup',getSuggestionsHanlder);
+  // });
+
+  let searchInput = document.getElementById('buscador');
+  const search = document.querySelector('#icon_buscador');
+
+
+  const getSearchTags = async (word) => {
+    try {
+        const suggestions = await fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=j4As5HO2OpUG2w2gTuuqQnIGuwOu2nnJ&limit=4&q=${word}`);
+        return suggestions.json();
+    } catch (error) {
+        console.log("ocurrio un error", e)
+    }
+}
+const gifosFound =[];
+
+const getGifosSearch = async (offset, query) => {
+    try {
+        const imagenes = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=j4As5HO2OpUG2w2gTuuqQnIGuwOu2nnJ&limit=12&offset=${offset}&q=${query}`);
+        return imagenes.json()
+    } catch (error) {
+        console.log("ocurrio un error", error)
+    }
+}
+
+
+// search suggestions
+const autocomplete = async (ev) => {
+  const containerList = document.querySelector('#match_list');
+  containerList.innerHTML= "";
+  if (ev.target.value.length >= 3) {
+      const tags = await getSearchTags(ev.target.value);
+      containerList.innerHTML = "";
+      tags.data.forEach(tag => {
           const newLi = document.createElement('li');
           newLi.textContent = tag.name;
-          contenedorSugerencias.appendChild(newLi);
-        });
-    }
-    if (ev.keyCode === 13) {
-      const gifs = await getSearchGifsByKeyword(API_KEY,ev.target.value);
-        gifs.data.forEach(gif => {
-          const newImg = document.createElement('img');
-          newImg.setAttribute("src",gif.images.fixed_height.url)
-          contenedorGifs.appendChild(newImg);
-        });
-    }
+          containerList.appendChild(newLi);
+
+          newLi.addEventListener("click", function() {
+              searchContent();
+              containerList.innerHTML = "";
+          })
+      })
   }
-  document.addEventListener('DOMContentLoaded',async () => {
-    document.querySelector('#buscador').addEventListener('keyup',getSuggestionsHanlder);
-  });
+  // search on enter
+  if (ev.key === 'Enter' || ev.keyCode === '13'){
+      const containerGifos = document.querySelector('#resultados_busqueda');
+      containerGifos.innerHTML = "";
+      containerList.innerHTML= "";
+      searchContent();
+  }
+}
+
+if (searchInput != null) {
+
+  searchInput.addEventListener('keyup', autocomplete);
+}
+
+let gifosOffset = 0;
+//getting input for search
+const searchContent = async () => {
+  const gifosSearch = await getGifosSearch(gifosOffset, searchInput.value);
+  fetchSearch(gifosSearch)
+}
+
+const viewMore = async() => {
+  gifosOffset +=12;
+  const gifosSearch = await getGifosSearch(gifosOffset, searchInput.value);
+  fetchSearch(gifosSearch)
+}
+
+if(search != null) {
+
+  search.addEventListener('click', searchContent);
+}
+
+
+
+// general search function
+
+const fetchSearch = (arr) => {
+
+  let searchResults = document.querySelector('#resultados_busqueda');
+  // searchResults.innerHTML = '';
+  if(arr.data.length === 0){
+      let noResultsTitle = document.createElement('h2');
+      noResultsTitle.innerText = searchInput.value;
+      
+      let noResultsImg = document.createElement('img');
+      noResultsImg.setAttribute('src', './assets/icon-busqueda-sin-resultado.svg');
+
+      let noResultsSuggestion = document.createElement('h3');
+      noResultsSuggestion.innerText = 'Intenta con otra búsqueda';
+
+      searchResults.append(noResultsTitle,noResultsImg,noResultsSuggestion);
+
+  } else {
+
+      let h2SearchResults = document.querySelector('#titulo_busqueda');
+      h2SearchResults.textContent = searchInput.value;
+      searchResults.prepend(h2SearchResults);
+      const containerGifos = document.querySelector('#resultados_busqueda')
+      arr.data.forEach(el => {
+          gifosFound.push(el);
+          const divGif = document.createElement('div');
+          divGif.classList.add('image');
+          const imageURL = el.images.fixed_height.url;
+          divGif.innerHTML = 
+          `<img class="imagenGif" src="${imageURL}" alt="${el.title}">
+          
+          <div class="dataGif">
+              <div class="botones">
+                  <a class="boton fav"><img data-id="${el.id}" src="./assets/icon-fav.svg" alt=""></a>
+                  <a class="boton maxGif"><img src="./assets/icon-max-normal.svg" alt=""></a>
+                  <a class="boton download" href="${imageURL}" download><img src="./assets/icon-download.svg" alt=""></a>
+              </div>
+              <div class="info">
+                  <p class="user">${el.username}</p>
+                  <h4 class="title">${el.title}</h4>
+              </div>
+          </div>`
+          containerGifos.appendChild(divGif);
+
+          // const maximise = divGif.querySelector('.dataGif > .botones > .maxGif');
+          // maximise.addEventListener('click', function() {
+          //     createModal(imageURL, el.title, el.username, el.id)
+          // });
+
+          // const image = divGif.querySelector('.imagenGif');
+          // image.addEventListener("click", function() {
+          //     createModal(imageURL, el.title, el.username, el.id)
+          // });
+
+          // divGif.querySelector('.fav').addEventListener('click', addToFavs)
+      })
+
+      searchInput.innerText = "";
+
+      //show view more button
+      let verMas = document.querySelector('#ver_mas');
+      verMas.style.display = "block";
+      verMas.addEventListener("click", viewMore)
+
+
+
+  }
+
+}
--- JS/trending.js
+++ JS/trending.js
@@ -1,18 +1,17 @@
-
+let arrayDescarga = [];
+const arrowLeft = document.querySelector("#left_arrow_id");
+const arrowRigth = document.querySelector("#rigth_arrow_id");
+const slick = document.getElementsByClassName("contenedor_gifos_hover");
 let contenedorCarrusel = document.querySelector('#contenedor_carrusel');
 let getApi = document.querySelector('.list_gifos');
 let leftArrow = document.querySelector('.left_arrow');
 let gifoUrl = "https://api.giphy.com/v1/gifs/trending?api_key=LcybAN2NSdMZKawiiuEU0m7lgBTrf52c&limit=12&offset=5&rating=g&random_id=e826c9fc5c929e0d6c6d423841a282aa";
-//modal 
 let contenedorModal = document.querySelector("#myModal");
 let modalImg = document.querySelector("#img01");
 let x = document.querySelector(".close");
 let optionsText = document.querySelector("#options");
 
-//modal
-const arrayDescarga = [];
-
-
+let l = 0;
 
 const CargarTrendings = async () => {
     try {
@@ -25,15 +24,12 @@ const CargarTrendings = async () => {
         console.log(error);
     }
 }
-
 const RenderizarTrendings = async () => {
     try {
         let trendings = await CargarTrendings()
         if (trendings) {
-
-            trendings.data.forEach(element => {
-                CrearCardTrending(element)
-            });
+            arrayDescarga = trendings.data
+            trendings.data.map(x => CrearCardTrending(x))
         }
 
     } catch (error) {
@@ -41,26 +37,20 @@ const RenderizarTrendings = async () => {
     }
 
 }
-
 const CrearCardTrending = (datosCard) => {
-    //lo vamos sacar
-
-    //Creamos Imagenes
     let imagenes = document.createElement('img');
     imagenes.setAttribute('src', datosCard.images.downsized.url);
+    imagenes.setAttribute('id', 'gif');
     imagenes.setAttribute('data-title', datosCard.title);
     imagenes.setAttribute('data-username', datosCard.username);
     imagenes.classList.add('trending');
-    imagenes.classList.add('gif');
-    //contenedorCarrusel.appendChild(imagenes)
-
-    let  contenedorGifosHover = document.createElement('div');
+    let contenedorGifosHover = document.createElement('div');
     contenedorGifosHover.setAttribute('class', 'contenedor_gifos_hover');
     contenedorGifosHover.appendChild(imagenes);
-    var mouseOverTarjeta = document.createElement('div');
+    let mouseOverTarjeta = document.createElement('div');
     mouseOverTarjeta.setAttribute('class', 'mouse_over_tarjeta2');
     mouseOverTarjeta.innerHTML = `
-    <div class="opciones_mouse_over">    
+        <div class="opciones_mouse_over">
                     <div class="borde_opciones_mause_over llegar_al_corazon">
                         <img class = guardar_favorito_corazon src="./assets/icon-fav-hover.svg" alt="">
                     </div>
@@ -69,49 +59,197 @@ const CrearCardTrending = (datosCard) => {
                     </div>
                     <div id="borde_opciones_mause_over_id" class="borde_opciones_mause_over">
                         <img src="./assets/icon-max-normal.svg"  class = "expandir_gifo_desktop" id ="expandir_gifo" alt="">
-                    </div>
-                    
+                    </div>                    
                 </div>
                 <div class="usuario2_mouse_over">
                     <h3 class ="gifo_usuario_hover">${datosCard.username}</h3>
                     <h2 class ="titulo_gifo_hover" class="titulo_gifo_hover">${datosCard.title}</h2>
-                </div>`;
-
-
+        </div>`;
     contenedorGifosHover.appendChild(mouseOverTarjeta);
     contenedorCarrusel.appendChild(contenedorGifosHover);
-    getApi.insertBefore(contenedorCarrusel, leftArrow);
-    // Evento click pop up modal
+    getApi.insertBefore(contenedorCarrusel, arrowRigth);
     imagenes.addEventListener("click", modal);
+}
+const modal = (e) => {
+    contenedorModal.style.display = "block";
+    let target = e.src ? e.src : e.target.src;
+    modalImg.setAttribute('src', target);
+    optionsText.style.display = "flex";
+    document.querySelector('#titulo_gifo').innerText = e.target.dataset.title;
+    document.querySelector('#gifo_usuario').innerText = e.target.dataset.username;
+    document.querySelector('#corazon2').addEventListener('click', () => {
+        let localStorageGifs = localStorage.getItem('favoritos');
+        let localStorageGifsArray = [];
+        if (localStorageGifs != null)
+            localStorageGifsArray = JSON.parse(localStorageGifs);
+        localStorageGifsArray.push(target)
+        localStorage.setItem('favoritos', JSON.stringify(localStorageGifsArray));
+    })
+}
+let MoverAlaDerecha = () => {
+    l++
+    for (var i of slick) {
+        if (l == 0) i.style.left = "0px";
+        if (l == 1) i.style.left = "-640px";
+        if (l == 2) i.style.left = "-1380px";
+        if (l == 3) i.style.left = "-2120px";
+        if (l == 4) i.style.left = "-2860px";
+        if (l > 4) l = 4
 
+    }
 
+}
+let MoverAlaIzquierda = () => {
+    l--
+    for (var i of slick) {
+        if (l == 0) i.style.left = "0px";
+        if (l == 1) i.style.left = "-740px";
+        if (l == 2) i.style.left = "-1480px";
+        if (l == 3) i.style.left = "-2220px";
+        if (l < 0) l = 4
 
+    }
+
+}
+let CerrarModal = () => {
+    contenedorModal.style.display = "none";
+}
+let expandirEscritorio = (elemento) => {
+
+    if (elemento.target.classList.contains('guardar_favorito_corazon')) {
+        Almacenar(elemento)
+        if (document.title == 'FAVORITOS') RenderizarFvoritos()
+
+    }
+
+    if (elemento.target.classList.contains('expandir_gifo_desktop'))
+        modalEscritorio(elemento.target.parentElement.parentElement.parentElement.parentElement.children[0]);
+
+}
+let Almacenar = (e) => {
+    let Src = e.target.parentElement.parentElement.parentElement.parentElement.children[0].src
+    let localStorageGifs = JSON.parse(localStorage.getItem('favoritos'));
+    let Auxiliar = localStorageGifs ? localStorageGifs : []
+    let verificacion = Auxiliar.filter(x => x == Src)
+    if (verificacion.length == 0) {
+        Auxiliar.push(Src)
+        localStorage.setItem('favoritos', JSON.stringify(Auxiliar));
+    }
+    else
+        console.log('Ya existe en favoritos');
 
 }
 
+let downloadGif = (evento) => {
+    let btnDownload = evento.target.parentElement.parentElement.parentElement.parentElement.children[0].getAttribute('data-title');
+    console.log(btnDownload);
+    if (evento.target.classList.contains('descargar_gifo_escritorio')) {
+        arrayDescarga.map(async (gif) => {
+            if (gif.title == btnDownload) {
+                await fetch(gif.images.downsized.url)
+                    .then((img) => {
+                        img.blob().then((file) => {
+                            let a = document.createElement("a");
+                            a.href = URL.createObjectURL(file);
+                            a.download = gif.title;
+                            a.click();
+                        });
+                    });
+            }
 
-const modal=(e)=>{
-    // console.log(e);
+        })
+
+    }
+}
+let modalEscritorio = (e) => {
     contenedorModal.style.display = "block";
-    let target= e.src?e.src:e.target.src;
+    let target = e.src ? e.src : e.target.src;
     modalImg.setAttribute('src', target);
-    optionsText.style.display = "flex";
-    // Title Gifo
-    document.querySelector('#titulo_gifo').innerText = e.target.dataset.title;
-    // User Gifo
-    document.querySelector('#gifo_usuario').innerText =e.target.dataset.username;
+    optionsText.style.display = "none";
+}
 
-    //save gif into local storage
-    document.querySelector('#corazon2').addEventListener('click', ()=> {
-        let localStorageGifs = localStorage.getItem('favoritos');
-        let localStorageGifsArray =  [];        
-        if (localStorageGifs != null) 
-            localStorageGifsArray = JSON.parse(localStorageGifs);
-        localStorageGifsArray.push(target)
-        localStorage.setItem('favoritos', JSON.stringify(localStorageGifsArray));        
+let RenderizarFvoritos = () => {
+    let localStorageGifsArray = [];
+    document.querySelector('#imagenes_favoritos').innerHTML = ''
+    let localStorageGifs = localStorage.getItem('favoritos');
+    if (localStorageGifs != null) {
+        localStorageGifsArray = JSON.parse(localStorageGifs);
+        document.querySelector('#conte_favoritos2').style.display = 'none'
+    }
 
+    localStorageGifsArray.map(x => {
+        let imagenes = document.createElement('img');
+        imagenes.setAttribute('src', x);
+        imagenes.classList.add('trending');
+        imagenes.classList.add('sacarFavoritos');
+        imagenes.setAttribute('id', 'gif');
+        imagenes.style.height = "100%";
+        imagenes.style.width = "100%";
+        imagenes.style.boxSizing = "border-box"
+
+        let contenedorGifosHover = document.createElement('div');
+        contenedorGifosHover.style.display = "flex"
+        contenedorGifosHover.style.justifyContent = "space-around"
+        contenedorGifosHover.setAttribute('class', 'contenedor_gifos_hover');
+        contenedorGifosHover.appendChild(imagenes);
+        let mouseOverTarjeta = document.createElement('div');
+        mouseOverTarjeta.style.height = "100%";
+        mouseOverTarjeta.style.width = "100%";
+        mouseOverTarjeta.style.right = "0";
+        mouseOverTarjeta.setAttribute('class', 'mouse_over_tarjeta2 ');
+        mouseOverTarjeta.innerHTML = `
+            <div class="opciones_mouse_over">
+                        <div class="borde_opciones_mause_over llegar_al_corazon">
+                            <img class = guardar_favorito_corazon src="./assets/icon-fav-hover.svg" alt="">
+                        </div>
+                        <div class="borde_opciones_mause_over click_descarga_gifo">
+                            <img class = "descargar_gifo_escritorio" id = "descargar_escritorio" src="./assets/icon-download.svg" alt="">
+                        </div>
+                        <div id="borde_opciones_mause_over_id" class="borde_opciones_mause_over">
+                            <img src="./assets/icon-max-normal.svg"  class = "expandir_gifo_desktop" id ="expandir_gifo" alt="">
+                        </div>                    
+                    </div>
+                 `;
+        contenedorGifosHover.appendChild(mouseOverTarjeta);
+        document.querySelector('#imagenes_favoritos').appendChild(contenedorGifosHover);
+    })
+    document.querySelector('#imagenes_favoritos').addEventListener('click', (e) => {
+
+        SacarDeFavoritos(e.target.parentElement.parentElement.parentElement.parentElement.children[0]);
     })
 }
 
 
-RenderizarTrendings()
+let main = () => {
+    if (document.title == 'FAVORITOS') {
+        RenderizarFvoritos()
+        RenderizarTrendings()
+    }
+    if (document.title == 'GIFOS HOME') {
+        RenderizarTrendings()
+    }
+}
+
+
+let SacarDeFavoritos = (e) => {
+    if (typeof e != "undefined") {
+        if (e.classList.contains('sacarFavoritos')) {
+            let localStorageGifs = JSON.parse(localStorage.getItem('favoritos'));
+            let Auxiliar = localStorageGifs ? localStorageGifs : []
+            let verificacion = Auxiliar.filter(item => item != e.src)
+            localStorage.setItem('favoritos', JSON.stringify(verificacion));
+            RenderizarFvoritos()
+        }
+
+    }
+}
+
+
+
+contenedorCarrusel?.addEventListener('click', downloadGif);
+contenedorCarrusel?.addEventListener('click', expandirEscritorio);
+arrowRigth?.addEventListener('click', MoverAlaDerecha)
+arrowLeft?.addEventListener('click', MoverAlaIzquierda)
+x?.addEventListener('click', CerrarModal)
+
+main()
